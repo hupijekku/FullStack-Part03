5b340290a1936c59e2c9409fd2de9819494bfa19
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-02-10 15:00:21 +0200

    Added frontend

---
+++ build/precache-manifest.95e53d5587cfacdcac03923a56f647d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01b92e52c0232196e7b3d03183a8507",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55445f1f0834440a95e1",
+    "url": "/static/js/2.731dfb59.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.731dfb59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a707bf5b05e1f766d08",
+    "url": "/static/js/main.f9a7ba9e.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 morgan.token('type', function(req){return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 
 let persons = [  
