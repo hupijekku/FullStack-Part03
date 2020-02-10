1b9c486b9c8f19b6303ebb1ac6bcaffba15417ff
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-02-10 14:54:38 +0200

    Part 3 Heroku

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('type', function(req){return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
-
 app.use(express.json())
+app.use(cors())
 
 let persons = [  
     {    
@@ -92,7 +93,7 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
